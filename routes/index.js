e1206040037951c6bcd450e8aa7fe50ab0ffab12
Alan Margolies <amargolies88@gmail.com>
2020-05-06 10:00:19 -0400

    created routes fixed view

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Eat-Da-Burger' });
+});
+
+// POST for creating burger
+router.post('/burgers/create', (req, res) => {
+  console.log(req.body.burgerName);
 });
 
 module.exports = router;
