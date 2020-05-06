533e66385c5e880bbcb59c025a81534b109b0a03
Alan Margolies <amargolies88@gmail.com>
2020-05-06 13:01:16 -0400

    added heorku connection conditional

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+let connection;
 
-connection.connect(function(err) {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
+
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
